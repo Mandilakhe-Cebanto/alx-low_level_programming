40dcd3a48f285d3bee880ef251c56d072e581c91
Mandilakhe-Cebanto <mandi.cebanto@gmail.com>
2022-09-10 23:57:40 -0700

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - print the string in the put function 
  *
  * Description: using the main function 
- * this program prints Programming is like building a mulitlingual puzzle
+ * this program prints "Programming is like building a mulitlingual puzzle
  * Return: 0
  */
 int main(void) 
