476c2787608a74e724e10cdb2179ee991a5e6bc1
Mandilakhe-Cebanto <mandi.cebanto@gmail.com>
2022-09-15 14:52:48 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
+	putchar('-');
+	num = -num;
 	}
 	if (num > 9)
 	{
-		print_number(num / 10);
+	print_number(num / 10);
 	}
 	putchar(num % 10 + '0');
 }
